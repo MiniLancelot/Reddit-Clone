069c42cccbf62e37276afec8de59bde01bb90867
Rakkoon <nhattnm.ilt@gmail.com>
2024-04-23 19:56:27 +0700

    add community list drawer

--- lib/router.dart
+++ lib/router.dart
@@ -3,6 +3,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:reddit_clone/features/auth/screens/login_screen.dart';
+import 'package:reddit_clone/features/community/screens/create_community_screen.dart';
 import 'package:reddit_clone/features/home/screens/home_screens.dart';
 import 'package:routemaster/routemaster.dart';
 
@@ -12,6 +13,7 @@ final loggedOutRoute =RouteMap(routes: {
 );
 
 final loggedInRoute =RouteMap(routes: {
-  '/': (_) => const MaterialPage(child: HomeScreen())
+  '/': (_) => const MaterialPage(child: HomeScreen()),
+  '/create-community': (_) => const MaterialPage(child: CreateCommunityScreen()),
   }
 );
