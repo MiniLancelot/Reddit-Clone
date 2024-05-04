928a2c7751c156353e7037060537988fcc7b5487
Duc Nguyen <vuducnguyen007@gmail.com>
2024-05-04 09:21:34 +0700

    fixing issue not showing anything in search query

--- lib/features/community/repository/community_repository.dart
+++ lib/features/community/repository/community_repository.dart
@@ -68,7 +68,7 @@ class CommunityRepository {
         isLessThan: query.isEmpty
           ? null
           : query.substring(0, query.length - 1) +
-              String.fromCharCode(query.codeUnitAt(query.length - 1) - 1))
+              String.fromCharCode(query.codeUnitAt(query.length - 1) + 1))
       .snapshots()
       .map((event) {
         List<Community> communities = [];
