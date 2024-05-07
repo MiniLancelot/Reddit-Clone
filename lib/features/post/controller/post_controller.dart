46299d6ce9377a41d920c36eea69fefca1b65377
unknown <vanminhho71@gmail.com>
2024-05-08 04:10:21 +0700

    Fetching Feed Posts

--- lib/features/post/controller/post_controller.dart
+++ lib/features/post/controller/post_controller.dart
@@ -20,6 +20,11 @@ final postControllerProvider =
       ref: ref);
 });
 
+final userPostsProvider = StreamProvider.family((ref, List<Community> communities) {
+  final postController = ref.watch(postControllerProvider.notifier);
+  return postController.fetchUserPosts(communities);
+});
+
 class PostController extends StateNotifier<bool> {
   final PostRepository _postRepository;
   final Ref _ref;
@@ -114,30 +119,35 @@ class PostController extends StateNotifier<bool> {
         path: 'posts/${selectedCommunity.name}', id: postId, file: file);
 
     imageRes.fold((l) => showSnackBar(context, l.message), (r) async {
-          final Post post = Post(
-      id: postId,
-      title: title,
-      communityName: selectedCommunity.name,
-      communityProfilePic: selectedCommunity.avatar,
-      upvotes: [],
-      downvotes: [],
-      commentCount: 0,
-      username: user.name,
-      uid: user.uid,
-      type: 'link',
-      createdAt: DateTime.now(),
-      awards: [],
-      link: r,
-    );
+      final Post post = Post(
+        id: postId,
+        title: title,
+        communityName: selectedCommunity.name,
+        communityProfilePic: selectedCommunity.avatar,
+        upvotes: [],
+        downvotes: [],
+        commentCount: 0,
+        username: user.name,
+        uid: user.uid,
+        type: 'image',
+        createdAt: DateTime.now(),
+        awards: [],
+        link: r,
+      );
 
-    final res = await _postRepository.addPost(post);
-    state = false;
-    res.fold((l) => showSnackBar(context, l.message), (r) {
-      showSnackBar(context, 'Posted Successfully!');
-      Routemaster.of(context).pop();
+      final res = await _postRepository.addPost(post);
+      state = false;
+      res.fold((l) => showSnackBar(context, l.message), (r) {
+        showSnackBar(context, 'Posted Successfully!');
+        Routemaster.of(context).pop();
+      });
     });
-    });
+  }
 
-
+  Stream<List<Post>> fetchUserPosts(List<Community> communities) {
+    if (communities.isNotEmpty) {
+      return _postRepository.fetchUserPosts(communities);
+    }
+    return Stream.value([]);
   }
 }
--- lib/features/post/repository/post_repository.dart
+++ lib/features/post/repository/post_repository.dart
@@ -5,6 +5,7 @@ import 'package:reddit_clone/core/constants/firebase_constants.dart';
 import 'package:reddit_clone/core/failure.dart';
 import 'package:reddit_clone/core/providers/firebase_provider.dart';
 import 'package:reddit_clone/core/type_defs.dart';
+import 'package:reddit_clone/models/community_model.dart';
 import 'package:reddit_clone/models/post_model.dart';
 
 final postRepositoryProvider = Provider((ref) {
@@ -29,4 +30,14 @@ class PostRepository {
     }
   }
 
+  Stream<List<Post>> fetchUserPosts(List<Community> communities) {
+    return _posts
+        .where('communityName',
+            whereIn: communities.map((e) => e.name).toList())
+        .orderBy('createdAt', descending: true)
+        .snapshots()
+        .map((event) => event.docs
+            .map((e) => Post.fromMap(e.data() as Map<String, dynamic>))
+            .toList(),);
+  }
 }
