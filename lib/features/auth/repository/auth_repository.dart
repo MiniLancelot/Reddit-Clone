7e3607b000bacddd22e1f8c37ed8b302bf855ea8
Rakkoon <nhattnm.ilt@gmail.com>
2024-06-07 13:54:47 +0700

    feat: google login while in guest status

--- lib/features/auth/repository/auth_repository.dart
+++ lib/features/auth/repository/auth_repository.dart
@@ -34,7 +34,7 @@ class AuthRepository {
 
   Stream<User?> get authStateChange => _auth.authStateChanges();
 
-  FutureEither<UserModel> signInWithGoogle() async {
+  FutureEither<UserModel> signInWithGoogle(bool isFromLogin) async {
     try {
       final GoogleSignInAccount? googleUser = await _googleSignIn.signIn();
 
@@ -45,8 +45,15 @@ class AuthRepository {
         idToken: googleAuth?.idToken,
       );
 
-      UserCredential userCredential =
-          await _auth.signInWithCredential(credential);
+      UserCredential userCredential;
+
+      if (isFromLogin) {
+          userCredential = await _auth.signInWithCredential(credential);
+        } else {
+          userCredential = await _auth.currentUser!.linkWithCredential(credential);
+        }
+
+      userCredential = await _auth.signInWithCredential(credential);
 
       UserModel userModel;
 
